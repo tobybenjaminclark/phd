eed0793944c39912c080a6d7cc9c0a806a205f30
Toby Clark <psytc9@nottingham.ac.uk>
2025-11-27 15:47:10 +0000

    add: walk over expressions

--- inference/pr_ast.py
+++ inference/pr_ast.py
@@ -33,6 +33,7 @@ class And(BooleanExpr):
     right: BooleanExpr
     def __str__(self): return f"{self.left} ∧ {self.right}"
     def __init__(self, l: BooleanExpr, r: BooleanExpr):  super().__init__(left=l, right=r)
+    def __iter__(self): return iter((self.left, self.right))
 
 
 class Or(BooleanExpr):
@@ -40,12 +41,14 @@ class Or(BooleanExpr):
     right: BooleanExpr
     def __str__(self): return f"{self.left} ∨ {self.right}"
     def __init__(self, l: BooleanExpr, r: BooleanExpr):  super().__init__(left=l, right=r)
+    def __iter__(self): return iter((self.left, self.right))
 
 
 class Not(BooleanExpr):
     inner: BooleanExpr
     def __str__(self): return f"¬ ({self.inner})"
     def __init__(self, i: BooleanExpr):  super().__init__(inner=i)
+    def __iter__(self): return iter((self.inner,))
 
 
 class Cmp(BooleanExpr):
@@ -54,6 +57,7 @@ class Cmp(BooleanExpr):
     right: ArithExpr
     def __str__(self): return f"{self.left} {self.op.value} {self.right}"
     def __init__(self, l: ArithExpr, o: CmpOp, r: ArithExpr):  super().__init__(left=l, op=o, right=r)
+    def __iter__(self): return iter((self.left, self.right))
 
 
 # Arithmetic Expressions
@@ -63,27 +67,40 @@ class Binary(ArithExpr):
     right: ArithExpr
     def __str__(self): return f"{self.left} {self.op.value} {self.right}"
     def __init__(self, l: ArithExpr, o: ArithOp, r: ArithExpr):  super().__init__(left=l, op=o, right=r)
+    def __iter__(self): return iter((self.left, self.right))
 
 class Number(ArithExpr):
     value: int
     def __str__(self): return str(self.value)
     def __init__(self, n: int):  super().__init__(value=n)
+    def __iter__(self): return iter(())
 
 class Symbol(ArithExpr):
     iden: str
     def __str__(self): return self.iden
     def __init__(self, i: str):  super().__init__(iden=i)
+    def __iter__(self): return iter(())
 
 
 
 rule = Not(
     Cmp(
-        Binary(Symbol("x"), ArithOp.ADD, Number(3)),
-        CmpOp.GT,
-        Binary(Symbol("y"), ArithOp.MUL, Number(2))
+        Symbol("r_i"),
+        CmpOp.GE,
+        Symbol("r_j")
     )
 )
 
 
 print(rule)
+
+def walk(expr):
+    yield expr
+    for child in expr:
+        yield from walk(child)
+
+
+for node in walk(rule):
+    print(type(node), node)
+
 
